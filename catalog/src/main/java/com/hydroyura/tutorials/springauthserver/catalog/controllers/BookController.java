0e91527e936e9c1e849d34c0c7bb56c6b4f29097
yury.klimchuk <hydro.yura@gmail.com>
2024-03-08 22:34:27 +0300

    catalog - updated

--- catalog/src/main/java/com/hydroyura/tutorials/springauthserver/catalog/controllers/BookController.java
+++ catalog/src/main/java/com/hydroyura/tutorials/springauthserver/catalog/controllers/BookController.java
@@ -1,18 +1,16 @@
 package com.hydroyura.tutorials.springauthserver.catalog.controllers;
 
-import com.hydroyura.tutorials.springauthserver.catalog.models.Book;
 import com.hydroyura.tutorials.springauthserver.catalog.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Role;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/books", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -27,6 +25,5 @@ public class BookController {
     public ResponseEntity<?> findAllWithLimit(JwtAuthenticationToken principal, @RequestParam(defaultValue = "10") Integer limit) {
         return new ResponseEntity<>(bookService.findAllWithLimit(limit), HttpStatus.OK);
     }
-
 
 }
